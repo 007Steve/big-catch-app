5251991a3001fa715e7e7cb63923f1e981cee72c
007steve <stephen.plummer1.sp@gmail.com>
2020-04-19 02:42:33 -0400

    add loginn and signup erb

---
+++ db/seed.rb
@@ -0,0 +1,6 @@
+User.create(name:"Jon",email:"jon@.com", password:"n24ns")
+User.create(name:"bob",email:"bob@.com", password:"n24cvbbfns")
+
+
+
+Catch.create(name:"bass",type:"smallmouth",length:"8",weight:"10")
