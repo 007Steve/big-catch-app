36ed356348edfbbfa886dc1618579761ecbc6370
007steve <stephen.plummer1.sp@gmail.com>
2020-04-19 03:40:18 -0400

    add session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,19 @@ class UsersController < ApplicationController
 
     post "/login" do 
         @user = User.find_by(email: params[:email])
-        @user.authenticate(params[:password])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "users/#{@user.id}"
+        else
+ 
+        end
     end
 
     get "/signup" do 
 
      end
+
+     get"/users/:id" do 
+     "show route"
+    end
 end
