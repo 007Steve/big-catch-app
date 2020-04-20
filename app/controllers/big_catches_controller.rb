f345f6225b0dbdaa0a333e4de3b64c403af79b6e
007steve <stephen.plummer1.sp@gmail.com>
2020-04-20 00:20:32 -0400

    added link to layout and modified bigcatch controller

--- app/controllers/big_catches_controller.rb
+++ app/controllers/big_catches_controller.rb
@@ -1,3 +1,20 @@
 class BigCatchesController < ApplicationController
     
+    get "/BigCatches" do
+        
+        erb :"/BigCatches/show"
+     end
+
+    get "/BigCatches/new" do
+        
+       erb :"/BigCatches/new"
+    end
+
+
+    post "" do 
+
+    end
+
+
+
 end
